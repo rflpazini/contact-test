6e33c760c9108bf4ce23e7fd841dd411dd797ef5
Rafael Pazini <rflpazini@gmail.com>
2016-12-04 03:22:02 -0200

    Complete CRUD

--- agenda/agenda.php
+++ agenda/agenda.php
@@ -5,15 +5,24 @@
 	<title>Contact Street</title>
 	<link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="../assets/css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<?php 
 		include '../imports/menu.php'
 	?>
 	<div class="container">
-		<button type="button" class="btn btn-primary btn-lg pull-right" data-toggle="modal" data-target="#myModal">
+		<button type="button" id="btn-add" class="btn btn-primary btn-lg pull-right" data-toggle="modal" data-target="#myModal">
 		  <i class="glyphicon glyphicon-plus"></i>
-		</button>	
+		</button>
+
+		<section class="row">
+			<div class="list-contacts">
+				<?php 
+					contactList();
+				?>
+			</div>
+		</section>	
 	</div>
 
 	
@@ -26,29 +35,63 @@
 	        <h4 class="modal-title" id="myModalLabel">Add new contact</h4>
 	      </div>
 	      <div class="modal-body">
-	        <form>
+	        <form role="form" method="POST" action="../config/add_contact.php">
 	        <div class="form-group">
-                <input type="text" class="form-control" id="name" placeholder="Name">
+                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
               </div>
               <div class="form-group">
-                <input type="email" class="form-control" id="email" placeholder="Email">
+                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
               </div>
               <div class="form-group">
-                <input type="text" class="form-control" id="address" placeholder="Address">
+                <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone Number">
               </div>
               <div class="form-group">
-                <input type="text" class="form-control" id="phone" placeholder="Phone Number">
+                <input type="text" class="form-control" id="address" name="address" placeholder="Address">
               </div>
+              <button type="submit" class="btn btn-primary pull-right">Save contact</button>
             </form>
 	      </div>
 	      <div class="modal-footer">
-	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-	        <button type="submit" class="btn btn-primary">Save contact</button>
 	      </div>
 	    </div>
 	  </div>
 	</div>
+	
+	<!-- Delete Modal -->
+	<div class="modal fade" id="modalDeleteContact" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Delete contact?</h4>
+          </div>
+          <div id="modalExcluirUsuario2" class="modal-body">
+            Doing this you will lose all contact information...
+
+
+            <input type="hidden" id="idContact"/>
+          </div>
+          <div class="modal-footer">
+            <button type="submit" class="btn btn-primary default" data-dismiss="modal" onclick="deleteContact()">Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
 	<script src="../bower_components/jquery/dist/jquery.min.js"></script>
 	<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+		function deleteContactModal(e){
+			$("#idContact").val(e.id);
+		}
+
+		function deleteContact(){
+			$.post(
+				"../config/delete_contact.php",
+				{idContact: $("#idContact").val()},
+				function(response){
+					window.location.href='../agenda/agenda.php';
+				});
+		}
+	</script>
 </body>
 </html>
--- config/functions.php
+++ config/functions.php
@@ -34,4 +34,69 @@ function sessionVerify() {
 	}
 }
 
+function contactList(){
+	include 'connect.php';
+	session_start();
+
+	$login = $_SESSION['login'];
+	
+	$sql = "SELECT * FROM contacts WHERE owner = '$login'" ;
+	$query = mysqli_query($connect, $sql);	
+
+	echo '
+		<div > 
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Phone</th>
+						<th>Address</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+				<tbody>';
+					while ($res = mysqli_fetch_assoc($query)){
+						echo '
+						<tr>
+							<td>'.$res['name'].'</td>
+							<td> '.$res['email'].'</td>
+							<td> '.$res['phone'].'</td>
+							<td> '.$res['address'].'</td>
+							<td> <button type="button" id="'.$res['id'].'" class="btn btn-danger" data-toggle="modal" data-target="#modalDeleteContact" onclick="deleteContactModal(this)"><span aria-hidden="true"><i class="fa fa-times fa-fw"></button></td>
+						</tr>';
+						
+					}
+					echo '
+				</tbody>
+			</table>
+		</div>';
+}
+
+function addContact() {
+	include 'connect.php';
+
+	session_start();
+	$owner = $_SESSION['login'];
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+	$address = $_POST['address'];
+
+	$sql = "INSERT INTO contacts(name, email, phone, address, owner) VALUES ('$name', '$email', '$phone', '$address', '$owner')";
+
+	$query = mysqli_query($connect, $sql);
+
+	echo"<script>window.location.href='../agenda/agenda.php';</script>";
+}
+
+function deleteContact() {
+	include 'connect.php';
+
+	$id = $_POST['idContact'];
+	$sql = "DELETE FROM contacts WHERE id = '$id'";
+	$query = mysqli_query($connect, $sql);
+}
+
 ?>
